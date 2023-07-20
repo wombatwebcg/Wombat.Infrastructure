1edc4f7861adefe694c339ae09fc72cb047f5ece
wombatwebcg <wombatwebcg@gmail.com>
2023-07-20 16:03:10 +0800

    增加多个WPF启动模板

--- Wombat.Infrastructure/ClassLibrary/InfrastructureTemplete.cs
+++ Wombat.Infrastructure/ClassLibrary/InfrastructureTemplete.cs
@@ -40,7 +40,45 @@ namespace Wombat.Infrastructure
             });
 
             //注入freesql
-            services.AddFreeSql(_connectionStrings);
+            services.AddFreeSql(_connectionStrings,true,true);
+
+            services.AddOptions();
+
+            services.AddServicesPoxy(assemblyNames);
+
+            services.BuildServiceProvider().UseCustomServiceProvider();
+
+        }
+
+        public static void SimpleConfigureServicesDebug(IServiceCollection services, Func<IConfiguration, object> otherConfiguration = null, params string[] assemblyNames)
+        {
+
+            LogHelper.Build();
+            ////register configuration
+            //IConfigurationBuilder cfgBuilder = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            //    //.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")}.json", optional: true, reloadOnChange:true)                             
+            //    ;
+            IConfiguration configuration = services.AddAppSettings();
+            configuration.UseCustomConfigurationProvider();
+            var _connectionStrings = configuration.GetSection("ConnectionStrings").Get<ConnectionStringsOptions>();
+
+            if (otherConfiguration != null)
+            {
+                services.AddSingleton(otherConfiguration.Invoke(configuration));
+            }
+
+            services.AddSingleton(new SnowflakeHelper(0, 0));
+
+            //register logger
+            services.AddLogging(build =>
+            {
+                object p = build.AddSerilog(logger: LogHelper.Log);
+            });
+
+            //注入freesql
+            services.AddFreeSql(_connectionStrings, true, isUseMonitorCommand:true);
 
             services.AddOptions();
 
