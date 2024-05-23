a02cfe64bee6e5c39f5582ad19d1a5f23efe3554
wombatwebcg <wombatwebcg@gmail.com>
2024-05-23 21:57:10 +0800

    减少依赖

--- Wombat.Infrastructure/ClassLibrary/InfrastructureTemplete.cs
+++ Wombat.Infrastructure/ClassLibrary/InfrastructureTemplete.cs
@@ -2,82 +2,81 @@
 using Serilog;
 using System;
 using Microsoft.Extensions.DependencyInjection;
-using Wombat.DependencyInjection;
 
 namespace Wombat.Infrastructure
 {
-    public class InfrastructureTemplete
-    {
+    //public class InfrastructureTemplete
+    //{
 
-        /// <summary>
-        /// 在DI容器中注册所有的服务类型 
-        /// </summary>
-        /// <param name="services"></param>
-        public static void SimpleConfigureServices(IServiceCollection services, Func<IConfiguration,object> otherConfiguration = null, params string[] assemblyNames)
-        {
+    //    /// <summary>
+    //    /// 在DI容器中注册所有的服务类型 
+    //    /// </summary>
+    //    /// <param name="services"></param>
+    //    public static void SimpleConfigureServices(IServiceCollection services, Func<IConfiguration,object> otherConfiguration = null, params string[] assemblyNames)
+    //    {
 
-            SerilogHelper.Build();
-            IConfiguration configuration = services.AddAppSettings();
-            configuration.UseCustomConfigurationProvider();
-            var  _connectionStrings = configuration.GetSection("ConnectionStrings").Get<ConnectionStringsOptions>();
+    //        SerilogHelper.Build();
+    //        IConfiguration configuration = services.AddAppSettings();
+    //        configuration.UseCustomConfigurationProvider();
+    //        var  _connectionStrings = configuration.GetSection("ConnectionStrings").Get<ConnectionStringsOptions>();
 
-            if (otherConfiguration != null)
-            {
-                services.AddSingleton(otherConfiguration.Invoke(configuration));
-            }
+    //        if (otherConfiguration != null)
+    //        {
+    //            services.AddSingleton(otherConfiguration.Invoke(configuration));
+    //        }
 
-            services.AddSingleton(new SnowflakeHelper(0, 0));
+    //        services.AddSingleton(new SnowflakeHelper(0, 0));
 
-            //register logger
-            services.AddLogging(build =>
-            {
-                object p = build.AddSerilog(logger: SerilogHelper.Log);
-            });
+    //        //register logger
+    //        services.AddLogging(build =>
+    //        {
+    //            object p = build.AddSerilog(logger: SerilogHelper.Log);
+    //        });
 
-            //注入freesql
-            services.AddFreeSql(_connectionStrings,true,true);
+    //        //注入freesql
+    //        services.AddFreeSql(_connectionStrings,true,true);
 
-            services.AddOptions();
+    //        services.AddOptions();
 
-            services.AddServices(assemblyNames);
+    //        services.AddServices(assemblyNames);
 
-            services.BuildServiceProvider().UseCustomServiceProvider();
+    //        services.BuildServiceProvider().UseCustomServiceProvider();
 
-        }
+    //    }
 
-        public static void SimpleConfigureServicesDebug(IServiceCollection services, Func<IConfiguration, object> otherConfiguration = null, params string[] assemblyNames)
-        {
+    //    public static void SimpleConfigureServicesDebug(IServiceCollection services, Func<IConfiguration, object> otherConfiguration = null, params string[] assemblyNames)
+    //    {
 
-            SerilogHelper.Build();
-            IConfiguration configuration = services.AddAppSettings();
+    //        SerilogHelper.Build();
+    //        IConfiguration configuration = services.AddAppSettings();
 
-            configuration.UseCustomConfigurationProvider();
+    //        configuration.UseCustomConfigurationProvider();
 
-            var _connectionStrings = configuration.GetSection("ConnectionStrings").Get<ConnectionStringsOptions>();
+    //        var _connectionStrings = configuration.GetSection("ConnectionStrings").Get<ConnectionStringsOptions>();
 
-            if (otherConfiguration != null)
-            {
-                services.AddSingleton(otherConfiguration.Invoke(configuration));
-            }
+    //        if (otherConfiguration != null)
+    //        {
+    //            services.AddSingleton(otherConfiguration.Invoke(configuration));
+    //        }
 
-            services.AddSingleton(new SnowflakeHelper(0, 0));
+    //        services.AddSingleton(new SnowflakeHelper(0, 0));
 
-            //register logger
-            services.AddLogging(build =>
-            {
-                object p = build.AddSerilog(logger: SerilogHelper.Log);
-            });
+    //        //register logger
+    //        services.AddLogging(build =>
+    //        {
+    //            object p = build.AddSerilog(logger: SerilogHelper.Log);
+    //        });
 
-            //注入freesql
-            services.AddFreeSql(_connectionStrings, true, isUseMonitorCommand:true);
+    //        //注入freesql
+    //        services.AddFreeSql(_connectionStrings, true, isUseMonitorCommand:true);
 
-            services.AddOptions();
+    //        services.AddOptions();
 
-            services.AddServices(assemblyNames);
+    //        services.AddServices(assemblyNames);
 
-            services.BuildServiceProvider().UseCustomServiceProvider();
+    //        services.BuildServiceProvider().UseCustomServiceProvider();
 
-        }
+    //    }
 
-    }
+    //}
 }
